1dd1bb5d58eba59ab95ce2ff70a2612131303e0d
hugo vilar <hugovilar04@gmail.com>
2024-04-10 15:03:57 -0300

    adiiconando tipagem e interpolação de objetos

--- SEÇÃO3/projeto-components/src/app/card/card.component.ts
+++ SEÇÃO3/projeto-components/src/app/card/card.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 
+interface IPlano {
+  infos: Iinfos;
+}
+
+interface Iinfos{
+  tipo:string,
+  preco:number
+}
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
 export class CardComponent {
-  plano = {
-    infos:{
-      tipo:'Simples',
-      preco:100
-    }
+  plano:IPlano = {
+    infos: {
+      tipo: 'Simples',
+      preco: 100.0,
+    },
   };
 }
