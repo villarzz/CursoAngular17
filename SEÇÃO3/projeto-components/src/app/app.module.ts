0ed75f7fb657a520d3700a150cbc215914649300
hugo vilar <hugovilar04@gmail.com>
2024-04-10 11:58:29 -0300

    adicioanndo html e css do card-roxo button

--- SEÇÃO3/projeto-components/src/app/app.module.ts
+++ SEÇÃO3/projeto-components/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { CardRoxoComponent } from './card-roxo/card-roxo.component';
 import { CardButtonComponent } from './card-button/card-button.component';
+import { CardRoxoButtonComponent } from './card-roxo-button/card-roxo-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
     CardRoxoComponent,
-    CardButtonComponent
+    CardButtonComponent,
+    CardRoxoButtonComponent
   ],
   imports: [
     BrowserModule,
