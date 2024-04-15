b0c2b5f4c098f31ffbe34d58d25ac0b78ca4f7d8
hugo vilar <hugovilar04@gmail.com>
2024-04-15 10:13:02 -0300

    aprendendo sobre eventBiding

--- SEÇÃO4/property-biding/src/app/app.component.ts
+++ SEÇÃO4/property-biding/src/app/app.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'property-biding';
+  inputText = 'texto inicial';
+  inputType = 'text';
+  isDisabled = false;
+
+  enableInput() {
+    this.isDisabled = false;
+  }
+
+  disableInput() {
+    this.isDisabled = true;
+  }
+
+  setPasswordType(){
+    this.inputType = 'password'
+  }
+
+  setTextType(){
+    this.inputType = 'text'
+  }
+
+  logInputText(){
+    console.log(this.inputText);
+  }
 }
