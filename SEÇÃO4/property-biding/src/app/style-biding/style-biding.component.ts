9eb51619de051bb0e706b5d27f2db3cd179d42b3
hugo vilar <hugovilar04@gmail.com>
2024-04-16 10:16:15 -0300

    propertybiding de estilos

--- SEÇÃO4/property-biding/src/app/style-biding/style-biding.component.ts
+++ SEÇÃO4/property-biding/src/app/style-biding/style-biding.component.ts
@@ -3,9 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-style-biding',
   templateUrl: './style-biding.component.html',
-  styleUrl: './style-biding.component.scss'
+  styleUrl: './style-biding.component.scss',
 })
 export class StyleBidingComponent {
-  widthButton1 = '110px'
-  widthButton2 = 120
+  widthButton1 = '110px';
+  widthButton2 = 120;
+  stylesObj = { width: '150px', backgroundColor: 'red' };
+
+  updateStyleObj(){
+    this.stylesObj = {
+      width: '150px',
+      backgroundColor:'red'
+    }
+  }
+
+  updateStyleObjCorrect(){
+    this.stylesObj = {
+      width: '160px',
+      backgroundColor:'grey'
+    }
+  }
+
+  updateWidth(){
+    this.widthButton2 = 130;
+  }
 }
