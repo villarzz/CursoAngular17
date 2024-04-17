1f0aa26b53459d330e39758834ac5527190fda78
hugo vilar <hugovilar04@gmail.com>
2024-04-17 09:39:01 -0300

    aprendendo sobre class-biding

--- SEÇÃO4/property-biding/src/app/class-biding/class-biding.component.ts
+++ SEÇÃO4/property-biding/src/app/class-biding/class-biding.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-class-biding',
   templateUrl: './class-biding.component.html',
-  styleUrl: './class-biding.component.scss'
+  styleUrl: './class-biding.component.scss',
 })
 export class ClassBidingComponent {
-
+  isRedButton = true;
+  isGreen = false;
 }
