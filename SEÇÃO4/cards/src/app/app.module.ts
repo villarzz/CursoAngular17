b63e6caf8b29dd210a0aac047133a1644f511cfc
hugo vilar <hugovilar04@gmail.com>
2024-04-18 14:18:27 -0300

    trocando button de basta

--- SEÇÃO4/cards/src/app/app.module.ts
+++ SEÇÃO4/cards/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './button/button.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
