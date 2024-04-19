ed33683f34adb631cd0b98e0f3124d3cbb8f283d
hugo vilar <hugovilar04@gmail.com>
2024-04-19 09:13:25 -0300

    gerando componente de card

--- SEÇÃO4/cards/src/app/app.module.ts
+++ SEÇÃO4/cards/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
