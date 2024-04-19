dbc28780be067b24581e6401f7b363041ef6963e
hugo vilar <hugovilar04@gmail.com>
2024-04-19 14:09:18 -0300

    aprendendo sobre ngif

--- SEÇÃO4/ngif/src/app/app.component.ts
+++ SEÇÃO4/ngif/src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ngif';
+  minhaProp = true;
+
+  toogleProp(){
+    this.minhaProp = !this.minhaProp
+  }
+
+  isTruthy(){
+    // tudo isso retorna true
+    // return {};
+    // return [];
+    // return true;
+    // return 1;
+    // return "teste";
+    // return "0";
+    // return "false";
+  }
 }
