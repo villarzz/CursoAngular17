f8b8c3d99a0aefbbb0433cc4fda58e06942c2bb5
hugo vilar <hugovilar04@gmail.com>
2024-04-19 15:07:50 -0300

    gewrandocomponente

--- SEÇÃO4/ngFor/src/app/app.component.ts
+++ SEÇÃO4/ngFor/src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'ngFor';
+  personSelectedIndex: number | undefined;
+
+  selectPerson(index: number) {
+    console.log(index);
+    this.personSelectedIndex = index
+  }
+
+  listPessoas = [
+    { name: 'Hugo Vilar', idade: 19 },
+    { name: 'Jorge Freitas', idade: 26 },
+    { name: 'Felipe Freitas', idade: 26 },
+    { name: 'João do taligado', idade: 31 },
+  ];
 }
