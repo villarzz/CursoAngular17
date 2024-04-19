fd29c4d54d5daa08b8f3e13e8000305385ff4971
hugo vilar <hugovilar04@gmail.com>
2024-04-19 10:22:08 -0300

    adicionando modulo de componentes

--- SEÇÃO4/cards/src/app/app.module.ts
+++ SEÇÃO4/cards/src/app/app.module.ts
@@ -1,22 +1,13 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './components/button/button.component';
-import { CardComponent } from './components/card/card.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ButtonComponent,
-    CardComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ComponentsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
