9b51f80195b7f16184338cf3b1d25bd350194f54
hugo vilar <hugovilar04@gmail.com>
2024-04-19 11:10:44 -0300

    aprendendo sobre ngmodel

--- SEÇÃO4/two-way/src/app/app.module.ts
+++ SEÇÃO4/two-way/src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [FormsModule, BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
