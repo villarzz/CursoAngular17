1f739df74db13af62b5bc4aa3dcd23a875e75864
hugo vilar <hugovilar04@gmail.com>
2024-04-22 11:17:52 -0300

    aprendendo sobre ngstyle

--- SEÇÃO4/ngStyle/src/app/app.component.ts
+++ SEÇÃO4/ngStyle/src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ngStyle';
+  fontSize: number = 15;
+  textColor: 'white' | 'orange' = 'orange';
+  buttonText: 'white' | 'orange' = 'white';
+  stylesString: string = 'font-size:15px; color:orange';
+  stylesObj = {
+    'font-size': this.fontSize + 'px',
+    color: this.textColor,
+  };
+
+  increaseFontSize() {
+    this.fontSize += 100;
+    this.stylesString = `font-size: ${this.fontSize}px; color: ${this.textColor}`;
+  }
+
+  toogleFontColor() {
+    if (this.textColor === 'white') {
+      this.textColor = 'orange';
+      this.buttonText = 'white';
+    } else {
+      this.textColor = 'white';
+      this.buttonText = 'orange';
+    }
+
+    this.stylesString = `font-size: ${this.fontSize}px; color: ${this.textColor}`;
+  }
 }
