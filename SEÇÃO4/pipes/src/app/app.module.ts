7913d838b71c2ebe27e21dc048eccc74cb23ecec
hugo vilar <hugovilar04@gmail.com>
2024-04-23 14:54:12 -0300

    aprendendo sobre pipes

--- SEÇÃO4/pipes/src/app/app.module.ts
+++ SEÇÃO4/pipes/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { StatusClassPipe } from './pipes/status-class.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatusClassPipe
   ],
   imports: [
     BrowserModule,
