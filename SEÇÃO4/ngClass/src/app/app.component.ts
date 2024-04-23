487056a02e8d7f2ef19280ddf1fcb4afe7293bbd
hugo vilar <hugovilar04@gmail.com>
2024-04-23 08:37:24 -0300

    aprendendo sobre ngclass

--- SEÇÃO4/ngClass/src/app/app.component.ts
+++ SEÇÃO4/ngClass/src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ngClass';
+
+  isGreen: boolean = true;
+
+  setToGreen() {
+    this.isGreen = true;
+  }
+
+  setToOrange() {
+    this.isGreen = false;
+  }
 }
