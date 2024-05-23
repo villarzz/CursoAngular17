bfc43694a56690650c556afe0e2542f3de4e73b0
hugo vilar <hugovilar04@gmail.com>
2024-05-23 11:12:55 -0300

    adicionando html e css basico

--- SEÇÃO5/projeto-5/src/app/app.module.ts
+++ SEÇÃO5/projeto-5/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { ComponentsModule } from './components/components.module';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AngularMaterialModule } from './angular-material/angular-material/angular-material.module';
 
 @NgModule({
   declarations: [
@@ -14,6 +14,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     BrowserModule,
     AppRoutingModule,
     ComponentsModule,
+    AngularMaterialModule,
   ],
   providers: [
     provideClientHydration(),
