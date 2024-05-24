e1b116f3d8401716a165e8471df5c9cbab275fbf
hugo vilar <hugovilar04@gmail.com>
2024-05-24 16:21:32 -0300

    adicionando componentes do filtro

--- SEÇÃO5/projeto-5/src/app/angular-material/angular-material/angular-material.module.ts
+++ SEÇÃO5/projeto-5/src/app/angular-material/angular-material/angular-material.module.ts
@@ -1,9 +1,30 @@
 import { NgModule } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @NgModule({
-  imports: [MatListModule, MatDividerModule],
-  exports: [MatListModule, MatDividerModule],
+  imports: [
+    MatListModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+  ],
+  exports: [
+    MatListModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+  ],
 })
 export class AngularMaterialModule {}
--- SEÇÃO5/projeto-5/src/app/components/components.module.ts
+++ SEÇÃO5/projeto-5/src/app/components/components.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FilterComponent } from './filter/filter.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { AngularMaterialModule } from '../angular-material/angular-material/angular-material.module';
 
 @NgModule({
-  declarations: [UserDetailsComponent],
-  imports: [AngularMaterialModule],
-  exports: [UserDetailsComponent],
+  declarations: [UserDetailsComponent, FilterComponent],
+  imports: [AngularMaterialModule, FormsModule],
+  exports: [UserDetailsComponent, FilterComponent],
 })
 export class ComponentsModule {}
