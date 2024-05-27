700c3724e3f89268ffe98829e748cb5e5a47b39e
hugo vilar <hugovilar04@gmail.com>
2024-05-27 17:21:08 -0300

    Adicionando componente de lista

--- SEÇÃO5/projeto-5/src/app/app.module.server.ts
+++ SEÇÃO5/projeto-5/src/app/app.module.server.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
-
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { ServerModule } from '@angular/platform-server';
 
 @NgModule({
   imports: [
--- SEÇÃO5/projeto-5/src/app/components/components.module.ts
+++ SEÇÃO5/projeto-5/src/app/components/components.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { FilterComponent } from './filter/filter.component';
-import { UserDetailsComponent } from './user-details/user-details.component';
-import { AngularMaterialModule } from '../angular-material/angular-material/angular-material.module';
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { FilterComponent } from "./filter/filter.component";
+import { UsersListComponent } from "./users-list/users-list.component";
+import { UserDetailsComponent } from "./user-details/user-details.component";
+import { AngularMaterialModule } from "../angular-material/angular-material/angular-material.module";
 
 @NgModule({
-  declarations: [UserDetailsComponent, FilterComponent],
+  declarations: [UserDetailsComponent, FilterComponent, UsersListComponent],
   imports: [AngularMaterialModule, FormsModule, CommonModule],
-  exports: [UserDetailsComponent, FilterComponent,CommonModule],
+  exports: [UserDetailsComponent, FilterComponent,CommonModule,UsersListComponent],
 })
 export class ComponentsModule {}
