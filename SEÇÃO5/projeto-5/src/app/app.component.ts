7e614212d288e9f89059da77653d42e6c7adbbfb
hugo vilar <hugovilar04@gmail.com>
2024-05-28 10:09:20 -0300

    interpolando input com html

--- SEÇÃO5/projeto-5/src/app/app.component.ts
+++ SEÇÃO5/projeto-5/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { IUser } from './interfaces/user/user.interface';
+import { UsersList } from './data/user-list';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'projeto-5';
+
+  userSelected: IUser = UsersList[0];
 }
