df8b1af3a72bea48df4855bc0b50c82c8be1b6ff
hugo vilar <hugovilar04@gmail.com>
2024-06-27 13:54:17 -0300

    Update app component HTML and TypeScript files

--- SEÇÃO6/template-variables/src/app/app.component.ts
+++ SEÇÃO6/template-variables/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  @ViewChild('meuInput') meuInput!: ElementRef<HTMLInputElement>;
 
+  updateInputText(){
+    this.meuInput.nativeElement.value = 'Texto atualizado!';
+  }
 }
