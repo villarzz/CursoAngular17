710277ac6be36b31ab61252783fcd5e9015885f5
hugo vilar <hugovilar04@gmail.com>
2024-06-27 09:59:36 -0300

    Add ChangeDetectorRef to AppComponent constructor and implement changeDetection() method

--- SEÇÃO6/template-variables/src/app/app.component.ts
+++ SEÇÃO6/template-variables/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'template-variables';
+
+  constructor(private _cdRef: ChangeDetectorRef) {}
+
+  changeDetection() {
+    this._cdRef.detectChanges();
+  }
 }
