eb90ddbffc82736efa05d168dea1ea9e9e7dfcea
hugo vilar <hugovilar04@gmail.com>
2024-07-17 11:14:17 -0300

    learning about elementref with viewChild

--- SEÇÃO6/projeto-element-ref/src/app/app.component.ts
+++ SEÇÃO6/projeto-element-ref/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'projeto-element-ref';
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('myDiv') divEl!: ElementRef<HTMLDivElement>;
+
+  constructor(private readonly _elRef: ElementRef){}
+
+  ngOnInit(){
+    const lastDivEl = this._elRef.nativeElement.querySelector('#other-div') as HTMLDivElement;
+    lastDivEl.textContent = 'I am the last div element';
+    lastDivEl.style.backgroundColor = 'lightblue';
+    lastDivEl.style.color = 'white';
+    lastDivEl.addEventListener('click', () => {
+      lastDivEl.textContent = 'I was clicked';
+    })
+    console.log(lastDivEl);
+  }
+
+  ngAfterViewInit(){
+    this.divEl.nativeElement.classList.add('my-class');
+    this.divEl.nativeElement.style.backgroundColor = 'orange';
+    this.divEl.nativeElement.textContent = 'I am a div element';
+  }
 }
