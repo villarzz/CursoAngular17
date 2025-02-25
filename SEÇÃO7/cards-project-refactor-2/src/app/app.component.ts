84fab1fa11644bbe7276730674ffaaf0d5d56b50
hugo vilar <hugovilar04@gmail.com>
2025-02-25 11:14:22 -0300

    refactor app component to use shared card component; remove individual card components

--- SEÇÃO7/cards-project-refactor-2/src/app/app.component.ts
+++ SEÇÃO7/cards-project-refactor-2/src/app/app.component.ts
@@ -1,17 +1,11 @@
 import { Component } from '@angular/core';
-import { DebitCardComponent } from './components/debit-card/debit-card.component';
-import { CreditCardComponent } from './components/credit-card/credit-card.component';
-import { BalanceCardComponent } from './components/balance-card/balance-card.component';
-import { ClientInfosCardComponent } from './components/client-infos-card/client-infos-card.component';
+import { CardComponent } from './shared/card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    BalanceCardComponent,
-    CreditCardComponent,
-    DebitCardComponent,
-    ClientInfosCardComponent,
+    CardComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
