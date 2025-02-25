e282d9df98226d10aeab0bf28255679503bb1ce4
hugo vilar <hugovilar04@gmail.com>
2025-02-25 09:27:09 -0300

    add credit card and balance card components with styles and tests

--- SEÇÃO7/cards-project-refactor/src/app/app.component.ts
+++ SEÇÃO7/cards-project-refactor/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { BalanceCardComponent } from './balance-card/balance-card.component';
+import { BalanceCardComponent } from './components/balance-card/balance-card.component';
+import { CreditCardComponent } from './components/credit-card/credit-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [BalanceCardComponent],
+  imports: [BalanceCardComponent, CreditCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'cards-project-refactor';
