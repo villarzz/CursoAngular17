d759f480ac4135859a5f85d1930519adcdd15834
hugo vilar <hugovilar04@gmail.com>
2025-02-25 11:14:20 -0300

    add card component with template, styles, and tests

---
+++ SEÇÃO7/cards-project-refactor-2/src/app/shared/card/card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+
+  @Input() icon = "";
+  @Input() title = "";
+  @Input() value = "";
+
+}
