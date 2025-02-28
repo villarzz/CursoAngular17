8281ce3691c3c25ae200c395263a295a7b9c4763
hugo vilar <hugovilar04@gmail.com>
2025-02-28 10:34:26 -0300

    feat: add color input to card text description directive and update template usage

--- SEÇÃO8/diretiva-com-seletor-elemento/src/app/components/card/directives/card-text-description.directive.ts
+++ SEÇÃO8/diretiva-com-seletor-elemento/src/app/components/card/directives/card-text-description.directive.ts
@@ -1,8 +1,10 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: 'app-card-text-description',
   host: { class: 'ca-u-card-text-description' },
   standalone: true,
 })
-export class CardTextDescriptionDirective {}
+export class CardTextDescriptionDirective {
+  @Input() @HostBinding('style.color') color: string = '';
+}
