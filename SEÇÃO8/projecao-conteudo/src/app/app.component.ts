6e6cc8660b1c47e2fe5023c886f195ede5e7b956
hugo vilar <hugovilar04@gmail.com>
2025-02-28 09:02:18 -0300

    feat: replace button and paragraph in app component with CompTextoComponent, update card component to support ng-content

--- SEÇÃO8/projecao-conteudo/src/app/app.component.ts
+++ SEÇÃO8/projecao-conteudo/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardComponent } from './card/card.component';
+import { CompTextoComponent } from "./comp-texto/comp-texto.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, CompTextoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
