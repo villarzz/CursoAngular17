b3b3532ab3eb46a7c6dd70bfa06001ae536b8b90
hugo vilar <hugovilar04@gmail.com>
2025-03-05 16:32:22 -0300

    feat: refactor card components to use app-card, app-card-header, and app-card-content directives; update styles for consistency

--- SEÇÃO9/projeto-cards-componentizado-completo/src/app/components/card/directives/card-header.directive.ts
+++ SEÇÃO9/projeto-cards-componentizado-completo/src/app/components/card/directives/card-header.directive.ts
@@ -1,7 +1,10 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: 'app-card-header',
   host: { class: 'ca-c-card__header' }
 })
-export class CardHeaderDirective {}
+export class CardHeaderDirective {
+
+  @Input() @HostBinding('style.backgroundColor') color: string = '';
+}
